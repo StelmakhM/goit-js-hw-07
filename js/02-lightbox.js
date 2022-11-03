f358fb668693e6b40a562846b49806ab89374fb9
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-04 00:35:05 +0200

    done 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryContainer: document.querySelector('.gallery'),
+}
+
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+refs.galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkUp);
+refs.galleryContainer.addEventListener('click', onClickOpenModal);
+
+
+function createGalleryMarkUp(items) {
+    return items.map(({ preview, original, description } = {}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    })
+        .join('')
+}
+
+
+function onClickOpenModal(event) {
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+    event.preventDefault();
+    let lightbox = new SimpleLightbox('.gallery a',
+        {
+            captions: true,
+            captionsData: 'alt',
+            captionDelay: 250,
+            captionPosition: 'bottom'
+        });
+}
