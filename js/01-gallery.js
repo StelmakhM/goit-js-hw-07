111519a1698d0ded4679a8dfe9a7665e695f0670
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-11-03 22:38:04 +0200

    started 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+    galleryContainer: document.querySelector('.gallery')
+}
+
+const galleryMarkUp = createGalleryMarkUp(galleryItems);
+refs.galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkUp);
+
+
+refs.galleryContainer.addEventListener('click', onImageClick);
+
+function createGalleryMarkUp(items) {
+    return items.map(({ preview, original, description } = {}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div> `
+    })
+        .join('')
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    createLightBox(event.target);
+}
+
+function createLightBox(img) {
+    const instance = basicLightbox.create(`
+        <img src="${img.dataset.source}" width="800" height="600">
+    `)
+    instance.show()
+}
+
+
+
+
